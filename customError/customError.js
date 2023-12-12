7bf23a3615b16e60e3f2cad3b79c7ed3a93b8c8b
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 16:07:24 +0100

    add internal error server that return a custom error class

--- customError/customError.js
+++ customError/customError.js
@@ -1,6 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
+
 class CustomError extends Error {
   constructor(message, status) {
     super(message);
     this.status = status;
   }
+}
+
+const internalServerError = (message) => {
+  return new CustomError("Something broke!.", StatusCodes.INTERNAL_SERVER_ERROR);
+}
+
+module.exports = {
+  internalServerError
 }
