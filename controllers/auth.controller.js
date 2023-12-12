bf7295e4d5963aa99a7e26fa1a635061dafe759d
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 17:35:34 +0100

    use CREATED constant instead of OK

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,12 +4,12 @@ const tryCatchWrapper = require("../middlewares/tryCatchWrapper");
 module.exports.register = tryCatchWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  res.status(StatusCodes.OK).json({
+  res.status(StatusCodes.CREATED).json({
     success: true,
     data: req.body
   });
 });
 
 module.exports.login = tryCatchWrapper(async (req, res, next) => {
-  res.status(StatusCodes.OK).json({ message: "to login endpoint" });
+  res.status(StatusCodes.CREATED).json({ message: "to login endpoint" });
 });
