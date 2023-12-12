7b273e315140b18154e4be902d8491e5bee99c02
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 17:42:56 +0100

    check for the empty values and return the badRequestError custom Error

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../middlewares/tryCatchWrapper");
+const { badRequestError } = require("../customError/customError");
 
 module.exports.register = tryCatchWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return next(badRequestError("username or email or password not provided!."))
+  }
 
   res.status(StatusCodes.CREATED).json({
     success: true,
