99fce6a46dcdbfc56f897ffcfd7eb1f4f9a161b7
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 17:34:36 +0100

    use StatusCodes Ok constant, and test body requests

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,15 @@
+const { StatusCodes } = require("http-status-codes");
 const tryCatchWrapper = require("../middlewares/tryCatchWrapper");
 
 module.exports.register = tryCatchWrapper(async (req, res, next) => {
-  res.status(200).json({ message: "to register endpoint" });
+  const { username, email, password } = req.body;
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: req.body
+  });
 });
 
 module.exports.login = tryCatchWrapper(async (req, res, next) => {
-  res.status(200).json({ message: "to login endpoint" });
+  res.status(StatusCodes.OK).json({ message: "to login endpoint" });
 });
