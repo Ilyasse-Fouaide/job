2d7e9298bfc4ef89d204fd0085d1c6a3397ed8d7
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 18:00:47 +0100

    generate a hashed password with bcrypt and save the user to the db

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
 const tryCatchWrapper = require("../middlewares/tryCatchWrapper");
 const { badRequestError } = require("../customError/customError");
+const User = require("../models/user.model");
 
 module.exports.register = tryCatchWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,9 +11,14 @@ module.exports.register = tryCatchWrapper(async (req, res, next) => {
     return next(badRequestError("username or email or password not provided!."))
   }
 
+  const salt = await bcrypt.genSalt(8);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ username, email, password: hashedPassword });
+
   res.status(StatusCodes.CREATED).json({
     success: true,
-    data: req.body
+    user
   });
 });
 
