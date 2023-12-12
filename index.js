b3f7b8b7494adfbd405d2bf8fa34f7f821fb49aa
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 16:56:55 +0100

    use job router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("./config/config");
 const authRouter = require("./routes/auth.router");
+const jobRouter = require("./routes/job.router");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
 const connect = require("./db/connect");
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1", authRouter);
+app.use("/api/v1/job", jobRouter);
 app.use(notFound);
 
 app.use(errorHandler);
