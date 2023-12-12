a35d8edc18ecd5f79a95510eb99537069515c9c1
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-12 16:15:55 +0100

    use Auth router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const config = require("./config/config");
+const authRouter = require("./routes/auth.router");
 
 const app = express();
+
+app.use("/api/v1", authRouter);
 
 const port = config.PORT || 5001;
 app.listen(port, () => console.log(`listening the port ${port}...`));
