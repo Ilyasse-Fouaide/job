0e20facf8baf2f708308abfadeac8cf1bbae391c
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 09:54:33 +0100

    Create job model and add create job functionality

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -1,4 +1,6 @@
+const Job = require("../models/job.model");
 const tryCatchWrapper = require("../middlewares/tryCatchWrapper");
+const { StatusCodes } = require("http-status-codes");
 
 module.exports.index = tryCatchWrapper(async (req, res, next) => {
   res.status(200).json({ message: "index" });
@@ -9,7 +11,16 @@ module.exports.show = tryCatchWrapper(async (req, res, next) => {
 });
 
 module.exports.store = tryCatchWrapper(async (req, res, next) => {
-  res.status(200).json({ message: "store" });
+  const job = await Job.create({
+    company: req.body.company,
+    position: req.body.position,
+    user: req.user.userId
+  });
+
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    job
+  });
 });
 
 module.exports.update = tryCatchWrapper(async (req, res, next) => {
