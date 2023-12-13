4ab13e26c0ee79e09eb8b2d57dbe9634f98a46e0
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 16:47:01 +0100

    Added standard headers and disabled legacy headers for rate limiting.

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const app = express();
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true,
+  legacyHeaders: false
 }));
 app.use(cors({
   origin: "*"
