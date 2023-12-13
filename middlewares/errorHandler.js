0b24008fc36a9f252a28a1109d31f5cceb25fcdb
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 16:05:51 +0100

    Update error handling status codes to use http-status-codes package

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,20 +2,20 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes")
 
 module.exports = (err, req, res, next) => {
   if (err.code && err.code === 11000) {
-    return res.status(400).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
       success: false,
       error: {
-        status: 400,
+        status: StatusCodes.BAD_REQUEST,
         message: `duplicate ${Object.keys(err.keyValue)}`
       }
     })
   }
 
   if (err.name && err.name == "ValidationError") {
-    return res.status(400).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
       success: false,
       error: {
-        status: 400,
+        status: StatusCodes.BAD_REQUEST,
         message: Object.values(err.errors).map(({ path, message }) => {
           return { [path]: message }
         })
@@ -24,10 +24,10 @@ module.exports = (err, req, res, next) => {
   }
 
   if (err.name && err.name === "CastError") {
-    return res.status(400).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
       success: false,
       error: {
-        status: 400,
+        status: StatusCodes.BAD_REQUEST,
         message: `Cast error for ObjectId ${err.value}`
       }
     })
