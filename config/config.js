5456715b2a499331bd6d73056e9eb3f034ba3215
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 18:41:33 +0100

    update the code and use env variable instead of passing the value

--- config/config.js
+++ config/config.js
@@ -6,5 +6,7 @@ module.exports = {
   "PORT": process.env.PORT,
   "MONGO_URI": process.env.MONGO_URI,
   "JWT_SECRET": process.env.JWT_SECRET,
-  "JWT_LIFETIME": process.env.JWT_LIFETIME
+  "JWT_LIFETIME": process.env.JWT_LIFETIME,
+  "RATE_LIMIT_TIME": process.env.RATE_LIMIT_TIME,
+  "RATE_LIMIT_MAX": process.env.RATE_LIMIT_MAX
 }
--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: parseInt(config.RATE_LIMIT_TIME),
+  max: parseInt(config.RATE_LIMIT_MAX),
+  message: "Too many requests, please try again later.",
   standardHeaders: true,
   legacyHeaders: false
 }));
