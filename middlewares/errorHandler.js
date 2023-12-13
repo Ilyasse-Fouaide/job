5a7ff7eb39b5c5f199cab9bade9abbb78b58a254
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 15:00:13 +0100

    Update error response status and message

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,8 +2,8 @@ module.exports = (err, req, res, next) => {
   res.status(err.status).json({
     success: false,
     error: {
-      status: err.status,
-      message: err.message
+      status: err.status || 500,
+      message: err.message || "Something broke!."
     }
   })
 }
