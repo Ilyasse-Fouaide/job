bf5e5a5b1f547860f8e0ab1e8673e42240c83859
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 09:05:56 +0100

    Add authorization middleware to job routes

--- routes/job.router.js
+++ routes/job.router.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const job = require("../controllers/job.controller");
+const authorize = require("../middlewares/authorize");
 
 router.route("/")
   .get(job.index)
-  .post(job.store);
+  .post(authorize, job.store);
 
 router.route("/:id")
   .get(job.show)
-  .patch(job.update)
-  .delete(job.destroy);
+  .patch(authorize, job.update)
+  .delete(authorize, job.destroy);
 
 module.exports = router
