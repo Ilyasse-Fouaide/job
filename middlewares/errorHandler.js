971f1ac739e6477429585ba36793079653a7dd89
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 15:15:13 +0100

    Update error status and message to use http-status-codes package

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,11 @@
+const { StatusCodes, ReasonPhrases } = require("http-status-codes")
+
 module.exports = (err, req, res, next) => {
   res.status(err.status).json({
     success: false,
     error: {
-      status: err.status || 500,
-      message: err.message || "Something broke!."
+      status: err.status || StatusCodes.INTERNAL_SERVER_ERROR,
+      message: err.message || ReasonPhrases.INTERNAL_SERVER_ERROR
     }
   })
 }
