df49beacb8f3e847801e943ffc6af0a182ac9af2
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-13 16:26:25 +0100

    Added CORS, XSS, and Helmet middleware to the Express app.

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 
+app.use(cors({
+  origin: "*"
+}));
+app.use(xss());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
